b44094fe9358a9800a22a3a4ba1b0fbee6f17fe6
André Oliveira <58999171+AndreOliveira23@users.noreply.github.com>
2023-06-09 19:50:37 -0300

    Add server.java
    Versão estável

---
+++ UDPServer.java
@@ -0,0 +1,41 @@
+package server.application;
+
+import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.*;
+import java.util.Random;
+import java.math.*;
+
+public class UDPServer {
+
+    public static void main(String[] args) throws Exception {
+while(true) {
+    try {
+        ServerSocket serverSocket = new ServerSocket(1234);
+
+        System.out.println("Aguardando conexão do cliente....");
+        Socket clientSocket = serverSocket.accept();
+        System.out.println("Cliente conectado!!");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+        //Le msg client
+        String mensagem = in.readLine();
+        System.out.println("Broadcast recebido pelo cliente em:" + mensagem);
+
+        out.println(String.valueOf(InetAddress.getLocalHost()));
+
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    } catch (IOException e) {
+
+    }
+}
+    }
+}
